24d331dc885311db7ac648a38c987232fd1760cd
Lucas Vayssières <lucas.vayssieres@student-cs.fr>
2025-11-03 15:39:44 +0100

    initial working build

---
+++ bundles/org.openhab.automation.monpetitbundle/src/main/java/org/openhab/automation/monpetitbundle/internal/MesPetitesConstants.java
@@ -0,0 +1,23 @@
+package org.openhab.automation.monpetitbundle.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * @author Robin Lacaze - initial contribution
+ */
+@NonNullByDefault
+public class MesPetitesConstants {
+    public static final String AUTOMATION_NAME = "pwm";
+
+    public static final String CONFIG_DUTY_CYCLE_ITEM = "dutycycleItem";
+    public static final String CONFIG_PERIOD = "interval";
+    public static final String CONFIG_MIN_DUTYCYCLE = "minDutycycle";
+    public static final String CONFIG_EQUATE_MIN_TO_ZERO = "equateMinToZero";
+    public static final String CONFIG_MAX_DUTYCYCLE = "maxDutycycle";
+    public static final String CONFIG_EQUATE_MAX_TO_HUNDRED = "equateMaxToHundred";
+    public static final String CONFIG_COMMAND_ITEM = "command";
+    public static final String CONFIG_DEAD_MAN_SWITCH = "deadManSwitch";
+    public static final String CONFIG_OUTPUT_ITEM = "outputItem";
+    public static final String INPUT = "input";
+    public static final String OUTPUT = "command";
+}
