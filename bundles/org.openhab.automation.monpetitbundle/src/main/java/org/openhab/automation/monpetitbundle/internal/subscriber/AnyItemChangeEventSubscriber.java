5404b7d34e67783b81a2698833e0b03882a65821
Lucas Vayssières <lucas.vayssieres@student-cs.fr>
2025-11-05 14:13:03 +0100

    Adds redirection to remote API

--- bundles/org.openhab.automation.monpetitbundle/src/main/java/org/openhab/automation/monpetitbundle/internal/subscriber/AnyItemChangeEventSubscriber.java
+++ bundles/org.openhab.automation.monpetitbundle/src/main/java/org/openhab/automation/monpetitbundle/internal/subscriber/AnyItemChangeEventSubscriber.java
@@ -14,12 +14,20 @@ package org.openhab.automation.monpetitbundle.internal.subscriber;
 
 import java.util.Collections;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+import javax.ws.rs.HttpMethod;
 
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.api.ContentResponse;
+import org.eclipse.jetty.client.util.StringContentProvider;
 import org.openhab.core.events.Event;
 import org.openhab.core.events.EventFilter;
 import org.openhab.core.events.EventSubscriber;
+import org.openhab.core.io.net.http.HttpClientFactory;
 import org.openhab.core.items.events.ItemStateChangedEvent;
 import org.openhab.core.thing.Thing;
 import org.openhab.core.thing.link.ItemChannelLinkRegistry;
@@ -29,21 +37,31 @@ import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 /**
  * @author lvcv4y initial contribution
  */
 @NonNullByDefault
 @Component(immediate = true, service = EventSubscriber.class)
 public class AnyItemChangeEventSubscriber implements EventSubscriber {
+    private static final Logger logger = LoggerFactory.getLogger(AnyItemChangeEventSubscriber.class);
+    public static final String API_URL = "http://localhost:7331/update";
 
     private final ItemChannelLinkRegistry itemChannelLinkRegistry;
+    private final HttpClient httpClient;
+    private final ObjectMapper objectMapper;
 
     @Activate
-    public AnyItemChangeEventSubscriber(@Reference ItemChannelLinkRegistry itemChannelLinkRegistry) {
+    public AnyItemChangeEventSubscriber(@Reference ItemChannelLinkRegistry itemChannelLinkRegistry,
+            @Reference HttpClientFactory httpClientFactory) {
         this.itemChannelLinkRegistry = itemChannelLinkRegistry;
-    }
+        this.httpClient = httpClientFactory.getCommonHttpClient();
 
-    private static final Logger logger = LoggerFactory.getLogger(AnyItemChangeEventSubscriber.class);
+        this.objectMapper = new ObjectMapper();
+    }
 
     @Override
     public Set<String> getSubscribedEventTypes() {
@@ -66,7 +84,33 @@ public class AnyItemChangeEventSubscriber implements EventSubscriber {
         Object newState = iscEvent.getItemState();
 
         for (Thing th : itemChannelLinkRegistry.getBoundThings(itemName)) {
-            logger.info("Item {} and linked to {} changed to {}", itemName, th.getLabel(), newState);
+            ObjectNode node = objectMapper.createObjectNode();
+            node.put("item_name", itemName);
+            node.put("thing_name", th.getLabel());
+            node.put("new_state", newState.toString());
+
+            String jsonPayload;
+
+            try {
+                jsonPayload = objectMapper.writeValueAsString(node);
+            } catch (JsonProcessingException e) {
+                logger.warn("Error serializing JSON: {}", e.getMessage());
+                continue;
+            }
+
+            try {
+                ContentResponse resp = httpClient.newRequest(API_URL).method(HttpMethod.POST)
+                        .header("Content-Type", "application/json")
+                        // .header("Authorization", "Bearer blabla")
+                        .content(new StringContentProvider(jsonPayload)).send();
+
+                if (resp.getStatus() >= 300) {
+                    logger.warn("HTTP code is not \"OK\": Got {} for payload {} at {}", resp.getStatus(), jsonPayload,
+                            API_URL);
+                }
+            } catch (ExecutionException | InterruptedException | TimeoutException e) {
+                logger.warn("Error executing POST request: {}", e.getMessage());
+            }
         }
     }
 }
